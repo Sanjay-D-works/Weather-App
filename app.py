88ea3be9fe7d2724ddcb05d363e650948b0fd968
Sanjay-D-works <sanjaydevarajworks@gmail.com>
2025-09-02 23:05:37 +0530

    creating an Sqlalchemy database

--- app.py
+++ app.py
@@ -5,7 +5,21 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
-    url = 
-    return render_template('weather.html')
+    url = 'https://pro.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=9e479c2c670336896fda8f9cdb407a34'
+    city = 'Las Vegas' 
+
+
+    r = requests.get(url.format(city)).json()
+    weather = {
+        'city' : city,
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+    }
+    print(weather)
+
+    return render_template('weather.html', weather=weather)
